a9aee4d02da7ffe7bcf729a0d12fea63e0fad644
Alessandro Tiberia <tiberia7030@gmail.com>
2026-04-28 14:53:13 +0200

    aggiunto css per header e main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,40 @@
-import 'Main.css';
+import './Main.css';
 
-function Main() { 
-    return <main>
+function Main() {
+    return <main className="blocco_principale">
 
-    <p>Content goes here </p>
-    <img src="/img/buy-comics-digital-comics.png" alt="" />
-    <img src="/img/buy-comics-merchandise.png" alt="" />
-    <img src="/img/buy-comics-subscriptions.png" alt="" />
-    <img src="/img/buy-comics-shop-locator.png" alt="" />
+        <div className="sezione_nera">
+            <p className="testo_segnaposto">--&gt; Content goes here &lt;--</p>
+        </div>
+        <div className="sezione_blu">
+            <div className="contenitore_servizi">
+             <div className="singolo_servizio">
+                        <img src="/img/buy-comics-digital-comics.png" alt="Digital Comics" className="immagine_servizio" />
+                        <span className="testo_servizio">DIGITAL COMICS</span>
+                    </div>
+                    
+                    <div className="singolo_servizio">
+                        <img src="/img/buy-comics-merchandise.png" alt="Merchandise" className="immagine_servizio" />
+                        <span className="testo_servizio">DC MERCHANDISE</span>
+                    </div>
+                    
+                    <div className="singolo_servizio">
+                        <img src="/img/buy-comics-subscriptions.png" alt="Subscriptions" className="immagine_servizio" />
+                        <span className="testo_servizio">SUBSCRIPTION</span>
+                    </div>
+                    
+                    <div className="singolo_servizio">
+                        <img src="/img/buy-comics-shop-locator.png" alt="Shop Locator" className="immagine_servizio" />
+                        <span className="testo_servizio">COMIC SHOP LOCATOR</span>
+                    </div>
+
+                    {/* Ho aggiunto la quinta icona presente nel layout originale */}
+                    <div className="singolo_servizio">
+                        <img src="/img/buy-dc-power-visa.svg" alt="Power Visa" className="immagine_servizio" />
+                        <span className="testo_servizio">DC POWER VISA</span>
+                    </div>
+            </div>
+        </div>
     </main>
 }
 export default Main;
